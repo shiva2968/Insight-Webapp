2296f18e40f0d0d0a8e65f241f95a3ce49a9d186
shiva2968 <41483865+shiva2968@users.noreply.github.com>
2020-06-05 01:21:29 -0500

    first commit

---
+++ flaskexample/views.py
@@ -0,0 +1,58 @@
+from flaskexample import app
+from flask import Flask, render_template, redirect, url_for, request
+from flask import render_template
+from werkzeug.exceptions import abort
+import cv2 as cv
+import numpy as np
+from PIL import Image
+import pytesseract
+
+#import re
+#import nltk
+#import heapq
+#import networkx as nx
+#from io import StringIO
+#from gensim.summarization import summarize
+
+
+
+
+#@app.route('/')
+#@app.route('/index')
+#def index():
+#  return "Hello, World!"
+
+#@app.route('/', methods=['GET', 'POST'])
+#def home():
+#    return render_template('upload.html')
+
+
+
+@app.route('/')  
+def upload():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/success', methods = ['POST'])  
+def success():  
+    if request.method == 'POST':  
+        f = request.files['file'] 
+        f.save(f.filename)
+        mm = image_to_sum(f.filename)   
+        return render_template("success.html", name = mm) #f.filename)  
+
+def image_to_sum(img):  
+      img = cv.imread(img)
+      gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+      sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+      sharpen = cv.filter2D(gray, -1, sharpen_kernel)
+      img = Image.fromarray(sharpen)
+      raw_text = pytesseract.image_to_string(img)
+      return raw_text
+
+
+
+
+
+
+
+
